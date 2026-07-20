3c2d8606b71baae9419cfb02c8de679d96beb7d5
Leonardo Cabral <leonardomcabral019@gmail.com>
2026-07-20 16:01:07 -0300

    ajustando parâmetro de sempre ter evidência de vídeo no arquivo de config do playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: 'https://loginxp.vercel.app/',
-    video: 'on-first-retry'
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
